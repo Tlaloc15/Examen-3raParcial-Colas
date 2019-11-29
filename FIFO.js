43fe8f0506b9465e86955cd33c5d53f3fd81f4d1
Carlos Eduardo Garcia Estrada <42812352+Tlaloc15@users.noreply.github.com>
2019-11-29 15:47:49 -0600

    commit

---
+++ FIFO.js
@@ -0,0 +1,22 @@
+class Nodo{
+    constructor(){
+        this._inicio = inicio;
+        this._siguiente = siguiente;
+        this._dato = dato;
+
+    }
+    extraer(){
+        let aux;
+        if(inicio == 0){
+            inicio.nuevo = aux.siguiente;
+        }
+        else{
+            aux.siguiente
+        }
+
+
+    }
+
+}
+
+//Colas, el primero en entrar es el primero en salir
